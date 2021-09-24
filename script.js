2c23643147ecb1e4d8d62d6ae965db9f94d9889d
mrpiyushprajapati <mrpiyushprajapati@gmail.com>
2021-09-24 23:47:31 +0530

    adding image slider

---
+++ script.js
@@ -0,0 +1,17 @@
+var index = 1;
+showDivs(index);
+
+function plusDivs(n) {
+  showDivs(index += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("img");
+  if (n > x.length) {index = 1}
+  if (n < 1) {index = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[index-1].style.display = "block";  
+}
